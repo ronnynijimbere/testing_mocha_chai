a714878c79c22380451e9c96507d2981abbe963a
Ronny <ronny.nijimbere86@gmail.com>
2020-08-26 08:39:12 +0200

    first commit

---
+++ dummy/students.js
@@ -0,0 +1,18 @@
+const students = [
+  {
+    id: 1,
+    name: "Ronny Nijimbere",
+    age: 34,
+  },
+  {
+    id: 2,
+    name: "Muzi Yeni",
+    age: 33,
+  },
+  {
+    id: 3,
+    name: "Smanga Khumalo",
+    age: 19,
+  },
+];
+export default students;
